2bd35ac3b04976cbe37a82bf42a8cedb4eaca45a
José Costa <88689761+Jose-Costa-M@users.noreply.github.com>
2023-09-24 16:31:40 -0600

    Update header.h

--- P1/header.h
+++ P1/header.h
@@ -5,3 +5,4 @@ void funTipoIP(unsigned char *IP, unsigned char *tipoIP, unsigned char clase);
 void funRed(unsigned char IP[4], unsigned char MR[4], unsigned char IPred[4]);
 void funcBroadcast(unsigned char IP[4], unsigned char MR[4], unsigned char IPbroad[4]);
 void funRango(unsigned char IPred[4], unsigned char IPbroad[4], unsigned char inferior[4], unsigned char superior[4]);
+void funElection(unsigned char bits,unsigned char *tipoIP);
